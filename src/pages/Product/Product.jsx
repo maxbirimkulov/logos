3773534da8cdb5f9a74c4f0e2be0adca30c204e4
Phantather <akdil.hstech@gmail.com>
2023-01-25 17:43:30 +0600

    add style for product page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -2,6 +2,9 @@ import React, {useEffect, useState} from 'react';
 import axios from "../../utils/axios";
 import {useNavigate, useParams} from "react-router-dom";
 
+import {IoIosArrowBack} from 'react-icons/io'
+import {BsCart3} from "react-icons/bs";
+
 const Product = () => {
 
     const [product, setProduct] = useState({})
@@ -23,8 +26,48 @@ const Product = () => {
     return (
         <section className='product'>
             <div className="container">
-                <button type='button' onClick={() => navigate(-1)}>back</button>
-                <h2>{product.title}</h2>
+                <button className="product__btn" type='button' onClick={() => navigate(-1)}>
+                    <span className="product__btn-icon">
+                        <IoIosArrowBack/>
+                    </span>
+                    Вернуться назад
+                </button>
+
+                <div className="product__content">
+                    <img src={`${product.image}`} alt="" className="product__content-img"/>
+                    <div className="product__info">
+                        <h2 className="product__info-title">{product.title}</h2>
+                        <p className="product__info-desc">{product.description}</p>
+                        <ul className="product__info-list">
+                            <li className="product__info-item">
+                                Белки: {product.protein}
+                            </li>
+                            <li className="product__info-item">
+                                Жиры: {product.fats}
+                            </li>
+                            <li className="product__info-item">
+                                Углеводы: {product.carbohydrates}
+                            </li>
+                            <li className="product__info-item">
+                                Ккал: {product.calories}
+                            </li>
+                            <li className="product__info-item">
+                                Вес: {product.weight} г
+                            </li>
+
+                        </ul>
+                        <div className="product__info-buy">
+                            <button className="products__card-btn header__btn">
+                                В корзину
+                                <BsCart3 size={20}/>
+                            </button>
+                            <p className="product__info-price">
+                                {product.price} ₽
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
     );
